21b612e2225c4bd5a7ee2d96b6644e68ed541050
vetematts <165625991+vetematts@users.noreply.github.com>
2026-05-03 19:23:25 +1000

    Fix service worker path in Pic2PDF app

--- tools/pic2pdf/app.js
+++ tools/pic2pdf/app.js
@@ -746,6 +746,6 @@ function hexToRgb(hex) {
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./sw.js").catch(() => {});
+    navigator.serviceWorker.register("../../sw.js").catch(() => {});
   });
 }
